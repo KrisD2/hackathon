4fa53de4d2566ab6a625ca85efb2450d0cdfa636
Kris Denert <kris.denert@gmail.com>
2019-11-21 19:50:41 -0700

    Styling

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -4,6 +4,6 @@ $(function() {
   $('.food').on('click', function() {
     $(this).fadeOut(1000);
     $(this).next().delay(1000).fadeIn(2000);
-    $('.food').off('click')
+    $('.food').off('click');
   });
 });
