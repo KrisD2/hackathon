8038adf223151099877e3343d83a65dec19627c1
Kris Denert <kris.denert@gmail.com>
2019-11-21 20:25:50 -0700

    js congrats

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -5,5 +5,9 @@ $(function() {
     $(this).fadeOut(1000);
     $(this).next().delay(1000).fadeIn(2000);
     $('.food').off('click');
+
+
+    $('body').children().delay(3000).hide();
+    $('.congrats').delay(5000).fadeIn(500);
   });
 });
