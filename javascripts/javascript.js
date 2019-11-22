ca51cea10923a85ad91801f5bce331d1d091a39c
Maria <mph5094@gmail.com>
2019-11-21 19:36:37 -0700

    Fixing animation

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,7 +1,7 @@
 // jshint esversion:6
 
 $(function() {
-  $('img').on('click', function() {
+  $('.food').on('click', function() {
     $(this).fadeOut(1000);
     $(this).next().delay(1000).fadeIn(2000);
   });
